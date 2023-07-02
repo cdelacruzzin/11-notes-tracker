d784885ffb2fce0f8e740412543296b0f9d8d713
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-01 22:39:22 -0400

    created a get('/:note_id') to get a specific note

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,33 +1,46 @@
 const notes = require('express').Router();
-const {v4: uuidv4} = require('uuid');
-
+const { v4: uuidv4 } = require('uuid');
 const {
     readFromFile,
     readAndAppend,
     writeToFile,
-  } = require('../helpers/fsUtils');
+} = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 
+notes.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+
+    readFromFile('./db/db.json') 
+        .then((data)=> JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id === noteId);
+            return result.length > 0
+            ? res.json(result)
+            : res.json('no note with that id');
+        })
+});
+
+
 notes.post('/', (req, res) => {
-    const { title, text} = req.body;
+    const { title, text } = req.body;
 
 
-if(req.body) {
-    const newNote = {
-        title,
-        text,
-        note_id: uuidv4(),
-    };
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
 
-    readAndAppend(newNotem, './db/db.json');
-    res.json('Note successfully added');
-} else {
-    res.error('Error in adding note');
-}
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note successfully added');
+    } else {
+        res.error('Error in adding note');
+    }
 })
 
 
